750fde36846f2c53e8c48102b436ec2e40ced3b5
Jose Kortelahti <jkortelahti@gmail.com>
2025-05-03 23:33:57 +0300

    Implement fmt.Stringer interface on Semver

--- semver.go
+++ semver.go
@@ -1,6 +1,10 @@
 package verso
 
-import "time"
+import (
+	"fmt"
+	"strings"
+	"time"
+)
 
 // Semver struct contains semantic version information
 type Semver struct {
@@ -10,4 +14,40 @@ type Semver struct {
 	PreRelease string
 	Build      string
 	Date       time.Time
+}
+
+// String implements [fmt.Stringer] interface. It returns Semver fields' values as a string.
+//
+// Major, Minor and Patch are mandatory fields and if all three are in their default values,
+// an empty string is returned. PreRelease, Build and Date fields are optional and will not be
+// printed if they are in their default values.
+func (s Semver) String() string {
+
+	if s.Major <= 0 && s.Minor <= 0 && s.Patch <= 0 {
+		return ""
+	}
+
+	sb := strings.Builder{}
+	sb.WriteString(fmt.Sprint(s.Major))
+	sb.WriteString(".")
+	sb.WriteString(fmt.Sprint(s.Minor))
+	sb.WriteString(".")
+	sb.WriteString(fmt.Sprint(s.Patch))
+
+	if s.PreRelease != "" {
+		sb.WriteString("-")
+		sb.WriteString(fmt.Sprint(s.PreRelease))
+	}
+
+	if s.Build != "" {
+		sb.WriteString("+")
+		sb.WriteString(fmt.Sprint(s.Build))
+	}
+
+	if !s.Date.IsZero() {
+		sb.WriteString(" ")
+		sb.WriteString(s.Date.Format(time.DateOnly))
+	}
+
+	return sb.String()
 }
---
+++ semver_test.go
@@ -0,0 +1,89 @@
+package verso_test
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/hkionline/verso"
+)
+
+func TestSemverString(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name     string
+		semver   verso.Semver
+		expected string
+	}{
+		{
+			name: "all fields",
+			semver: verso.Semver{
+				Major:      1,
+				Minor:      1,
+				Patch:      2,
+				PreRelease: "beta",
+				Build:      "002",
+				Date:       time.Date(2023, 10, 1, 0, 0, 0, 0, time.UTC),
+			},
+			expected: "1.1.2-beta+002 2023-10-01",
+		},
+		{
+			name: "default Major.Minor.Patch",
+			semver: verso.Semver{
+				Major: 0,
+				Minor: 0,
+				Patch: 0,
+			},
+			expected: "",
+		},
+		{
+			name: "empty PreRelease field",
+			semver: verso.Semver{
+				Major: 1,
+				Minor: 1,
+				Patch: 2,
+				Build: "002",
+				Date:  time.Date(2023, 10, 1, 0, 0, 0, 0, time.UTC),
+			},
+			expected: "1.1.2+002 2023-10-01",
+		},
+		{
+			name: "empty Build field",
+			semver: verso.Semver{
+				Major:      1,
+				Minor:      1,
+				Patch:      2,
+				PreRelease: "beta",
+				Date:       time.Date(2023, 10, 1, 0, 0, 0, 0, time.UTC),
+			},
+			expected: "1.1.2-beta 2023-10-01",
+		},
+		{
+			name: "default Date field",
+			semver: verso.Semver{
+				Major:      1,
+				Minor:      1,
+				Patch:      2,
+				PreRelease: "beta",
+				Build:      "002",
+				Date:       time.Time{},
+			},
+			expected: "1.1.2-beta+002",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			testStringer(test.semver, test.expected, t)
+		})
+	}
+}
+
+func testStringer(s fmt.Stringer, expected string, t *testing.T) {
+	actual := s.String()
+
+	if actual != expected {
+		t.Fatalf("expected \"%s\" but got \"%s\"\n", expected, actual)
+	}
+}
